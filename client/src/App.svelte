820e3057188c83f35740a4389f1d8e8bfe7457a9
gustavtjac <gustavtorup@gmail.com>
2026-05-06 14:46:32 +0200

    public profile ready to be created

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,27 +1,47 @@
 <script>
-import { onMount } from 'svelte';
-import { Toaster } from "svelte-sonner";
-import { Router, Route } from "svelte-routing";
-import Home from "./pages/Homepage/Home.svelte";
-import Auth from "./pages/Auth/Auth.svelte";
-import Landing from "./pages/Landing/Landing.svelte";
-import RouteGuard from './components/RouteGuard/RouteGuard.svelte';
-import { checkAuth } from './stores/userStore.js';
-import Profile from './pages/Profile/Profile.svelte';
-import Leaderboard from './pages/Leaderboard/Leaderboard.svelte';
+  import { onMount } from "svelte";
+  import { Toaster } from "svelte-sonner";
+  import { Router, Route } from "svelte-routing";
+  import Home from "./pages/Homepage/Home.svelte";
+  import Auth from "./pages/Auth/Auth.svelte";
+  import Landing from "./pages/Landing/Landing.svelte";
+  import RouteGuard from "./components/RouteGuard/RouteGuard.svelte";
+  import { checkAuth } from "./stores/userStore.js";
+  import Profile from "./pages/Profile/Profile.svelte";
+  import Leaderboard from "./pages/Leaderboard/Leaderboard.svelte";
+  import PublicProfile from "./pages/PublicProfile/PublicProfile.svelte";
 
-onMount(() => checkAuth());
-
+  onMount(() => checkAuth());
 </script>
-<Toaster/>
+
+<Toaster />
 <Router>
   <div>
-    <Route path='/'><Landing/></Route>
-    <Route path='/login'><RouteGuard requireAuth={false}><Auth view="login"/></RouteGuard></Route>
-    <Route path='/register'><RouteGuard requireAuth={false}><Auth view="register"/></RouteGuard></Route>
-    <Route path='/dashboard'><RouteGuard requireAuth={true}><Home/></RouteGuard></Route>
-    <Route path='/profile'><RouteGuard requireAuth={true}><Profile/></RouteGuard></Route>
-    <Route path='/leaderboard'><RouteGuard requireAuth={true}><Leaderboard/></RouteGuard></Route>
-    <Route><h1>404</h1><p>This page does not exist.</p></Route>
+    <Route path="/"><Landing /></Route>
+    <Route path="/login"
+      ><RouteGuard requireAuth={false}><Auth view="login" /></RouteGuard></Route
+    >
+    <Route path="/register"
+      ><RouteGuard requireAuth={false}><Auth view="register" /></RouteGuard
+      ></Route
+    >
+    <Route path="/dashboard"
+      ><RouteGuard requireAuth={true}><Home /></RouteGuard></Route
+    >
+    <Route path="/profile"
+      ><RouteGuard requireAuth={true}><Profile /></RouteGuard></Route
+    >
+    <Route path="/profile/:username" let:params>
+      <RouteGuard requireAuth={true}>                                                                                                     
+          <PublicProfile username={params.username} />                       
+      </RouteGuard>                      
+  </Route>
+    <Route path="/leaderboard"
+      ><RouteGuard requireAuth={true}><Leaderboard /></RouteGuard></Route
+    >
+    <Route
+      ><h1>404</h1>
+      <p>This page does not exist.</p></Route
+    >
   </div>
 </Router>
---
+++ client/src/pages/PublicProfile/PublicProfile.svelte
@@ -0,0 +1,13 @@
+<script>
+import LeaderboardBanner from "../../components/LeadearboardBanner/LeaderboardBanner.svelte";
+import Navbar from "../../components/Nav/Navbar.svelte";
+
+let { username } = $props();      
+
+</script>
+
+<LeaderboardBanner/>
+<Navbar/>
+
+
+<h2>PUBLIC {username}</h2>
