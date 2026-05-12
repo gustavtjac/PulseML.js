852bfd6143c8011be99f779fe798f6beb435de11
gustavtjac <gustavtorup@gmail.com>
2026-05-12 12:50:57 +0200

    datautil to calc age based on birthday

---
+++ client/src/util/dateUtil.js
@@ -0,0 +1,8 @@
+export function calcAge(birthday) {
+    const today = new Date();
+    const born = new Date(birthday);
+    let age = today.getFullYear() - born.getFullYear();
+    const monthDiff = today.getMonth() - born.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < born.getDate())) age--;
+    return age;
+}
